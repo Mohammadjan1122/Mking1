f0c6d7079ea9f09ac67bdcb2dd47c8df48e41089
Mohammadjan1122 <63337310+Mohammadjan1122@users.noreply.github.com>
2022-02-27 01:02:19 +0430

    Update run_mking.py

--- run_mking.py
+++ run_mking.py
@@ -1,4 +1,18 @@
 import os 
 os.system("git pull")
-from mking1 import mking_menu
-mking_menu() 
+import os, platform
+try:
+   import requests
+except:
+   os.system('pip2 install requests')
+
+import requests
+os.system("git pull") 
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from mking1 import mking_menu
+    mohammad()
+elif bit == '32bit':
+    from mking1 import mking_menu
+    mohammad()
+ 
